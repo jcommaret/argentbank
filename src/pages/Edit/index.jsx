5d279a9cef2770a9db86fbe6d55418eaca59c996
jcommaret@gmail.com <jcommaret@gmail.com>
2023-03-29 18:13:33 +0200

    Update JSX

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -1,7 +1,56 @@
+import { Link } from "react-router-dom"
+
+const accounts = {
+  title: "Accounts",
+  desc: [
+    {
+      title: "Argent Bank Checking (x8349)",
+      amount: "$2,082.79",
+      amountDescription: "Available Balance",
+    },
+    {
+      title: "Argent Bank Savings (x6712)",
+      amount: "$10,928.42",
+      amountDescription: "Available Balance",
+    },
+    {
+      title: "Argent Bank Credit Card (x8349)",
+      amount: "$184.30",
+      amountDescription: "Current Balance",
+    },
+  ],
+}
+
 export default function Edit() {
   return (
-    <div>
-      <h1>Edit</h1>
-    </div>
+    <main class="main bg-dark">
+      <div class="header">
+        <h1>
+          Welcome back
+          <br />
+          Tony Jarvis!
+        </h1>
+        <button class="save-button">Save</button>
+        <button class="cancel-button">Cancel</button>
+      </div>
+
+      <h2 class="sr-only">{accounts.title}</h2>
+      {accounts.desc.map((account) => (
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">{account.title}</h3>
+            <p className="account-amount">{account.amount}</p>
+            <p className="account-amount-description">
+              {account.amountDescription}
+            </p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <Link to="transactions">
+              <button className="transaction-button">View transactions</button>
+            </Link>
+          </div>
+        </section>
+      ))}
+    </main>
   )
 }
--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,3 +1,26 @@
+import { Link } from "react-router-dom"
+
+const accounts = {
+  title: "Accounts",
+  desc: [
+    {
+      title: "Argent Bank Checking (x8349)",
+      amount: "$2,082.79",
+      amountDescription: "Available Balance",
+    },
+    {
+      title: "Argent Bank Savings (x6712)",
+      amount: "$10,928.42",
+      amountDescription: "Available Balance",
+    },
+    {
+      title: "Argent Bank Credit Card (x8349)",
+      amount: "$184.30",
+      amountDescription: "Current Balance",
+    },
+  ],
+}
+
 export default function User() {
   return (
     <main class="main bg-dark">
@@ -7,39 +30,28 @@ export default function User() {
           <br />
           Tony Jarvis!
         </h1>
-        <button class="edit-button">Edit Name</button>
+        <Link to="edit">
+          <button class="edit-button">Edit Name</button>
+        </Link>
       </div>
-      <h2 class="sr-only">Accounts</h2>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Checking (x8349)</h3>
-          <p class="account-amount">$2,082.79</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Savings (x6712)</h3>
-          <p class="account-amount">$10,928.42</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p class="account-amount">$184.30</p>
-          <p class="account-amount-description">Current Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
+
+      <h2 class="sr-only">{accounts.title}</h2>
+      {accounts.desc.map((account) => (
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">{account.title}</h3>
+            <p className="account-amount">{account.amount}</p>
+            <p className="account-amount-description">
+              {account.amountDescription}
+            </p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <Link to="transactions">
+              <button className="transaction-button">View transactions</button>
+            </Link>
+          </div>
+        </section>
+      ))}
     </main>
   )
 }
