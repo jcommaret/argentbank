6625dc5994b9f714f4db1192934f9169d354e66c
jcommaret@gmail.com <jcommaret@gmail.com>
2023-03-30 13:29:40 +0200

    Sign in / component

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,31 +1,9 @@
-import { Link } from "react-router-dom"
-
-import "./index.scss"
+import Sign from "../../components/Sign"
 
 export default function SignIn() {
   return (
     <main class="main bg-dark">
-      <section class="sign-in-content">
-        <i class="fa fa-user-circle sign-in-icon"></i>
-        <h1>Sign In</h1>
-        <form>
-          <div class="input-wrapper">
-            <label for="username">Username</label>
-            <input type="text" id="username" />
-          </div>
-          <div class="input-wrapper">
-            <label for="password">Password</label>
-            <input type="password" id="password" />
-          </div>
-          <div class="input-remember">
-            <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
-          </div>
-          <Link to="../user/12" class="sign-in-button">
-            Sign In
-          </Link>
-        </form>
-      </section>
+      <Sign />
     </main>
   )
 }
